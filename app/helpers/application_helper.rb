0c06d35a19c309389e1889cfb3a9fd9c99153268
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-05 17:45:42 +0100

    created SVG helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,19 @@
 module ApplicationHelper
+  require 'nokogiri'
+
+  # Helper method for displaying SVGs
+  # If a URL is provided manipulate the SVG wrapping it into a clickable link.
+
+  def svg(image, url=nil)
+    file_path = "#{Rails.root}/app/assets/images/svg/#{image}"
+    if url
+      doc = Nokogiri::XML(File.read(file_path))
+      children = doc.root.children
+      doc.root.children = "<a href='#{url}'>"
+      doc.at("a").children = children
+      doc.to_s.html_safe
+    else
+      File.read(file_path).html_safe
+    end
+  end
 end
