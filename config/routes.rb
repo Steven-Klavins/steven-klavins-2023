1efc4d9faffa8a70e9208b43c5206593be0d3585
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-08 00:02:40 +0100

    added category and blog routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
+  resources :blogs do
+    get '/page/:page', action: :index, on: :collection
+  end
+
   root to: 'pages#home'
+
   get 'contact', to: 'pages#contact', as: 'contact'
   get 'about', to: 'pages#about', as: 'about'
 end
