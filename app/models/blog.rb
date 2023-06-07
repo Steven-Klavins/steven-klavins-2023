4c49854a78e039ac309735562721b647ffd2035d
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-07 23:44:57 +0100

    added blog relationships and validation

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,4 +1,10 @@
 class Blog < ApplicationRecord
-  has_rich_text :body
+  validates_presence_of :title, :body
+
+  has_many :blogs_categories
+  has_many :categories, through: :blogs_categories
   has_one_attached :cover_image, dependent: :destroy
+
+  has_rich_text :body
+  paginates_per 5
 end
