0fb4fb4cf8ace96f8d1325fdda762f2277e93445
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-08 19:27:36 +0100

    added friendly id and pagination to category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,16 @@
 class Category < ApplicationRecord
-  has_many :blogs_categories
-  has_many :blogs, through: :blogs_categories
+  # Friendly ID
+  extend FriendlyId
+  friendly_id :name, use: %i(slugged history finders)
+
+  # Validation
   validates_presence_of :name
   validates :name, uniqueness: true
+
+  # Relationships
+  has_many :blogs_categories
+  has_many :blogs, through: :blogs_categories
+
+  # UI
+  paginates_per 5
 end
