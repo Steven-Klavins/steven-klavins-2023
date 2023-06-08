55d20bd4de57aa8af9a5bb9f36d15130d61979af
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-08 19:26:15 +0100

    added friendly id to blog

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,11 +1,18 @@
 class Blog < ApplicationRecord
+  # Friendly ID
+  extend FriendlyId
+  friendly_id :title, use: %i(slugged history finders)
+
+  # Validation
   validates_presence_of :title, :body
   validates :title, length: { maximum: 60 }
 
+  # Relationships
   has_many :blogs_categories
   has_many :categories, through: :blogs_categories
   has_one_attached :cover_image, dependent: :destroy
 
+  # UI
   has_rich_text :body
   paginates_per 5
 end
