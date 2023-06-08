16b6f0c2af9fa60cb6e5a4e4a160a9379b3e483a
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-08 14:17:04 +0100

    added validation to check name unique

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,5 @@ class Category < ApplicationRecord
   has_many :blogs_categories
   has_many :blogs, through: :blogs_categories
   validates_presence_of :name
+  validates :name, uniqueness: true
 end
