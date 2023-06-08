87be53b79f7565d3acf1e68943e03d9368dcb348
Steven-Klavins <stevenklavins94@gmail.com>
2023-06-08 19:30:43 +0100

    routed category to /blogs/category

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :categories
+  resources :categories, path: '/blogs/category' do
+    get '/page/:page', action: :index, on: :collection
+  end
+
   resources :blogs do
     get '/page/:page', action: :index, on: :collection
   end
