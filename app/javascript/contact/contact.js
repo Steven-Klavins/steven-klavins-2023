cd558f99434fcfff0dfa936275f61902c99c0630
Steven-Klavins <stevenklavins94@gmail.com>
2023-07-07 22:56:11 +0100

    added form validation

--- app/javascript/contact/contact.js
+++ app/javascript/contact/contact.js
@@ -38,35 +38,75 @@ function flashCopyEvent() {
 
 // Validate the form email and text inputs
 function validateContactForm(formInputs, textInputs) {
-    let validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
     let is_valid = true;
+
     formInputs.forEach(input => {
-        if (input.type !== "submit" && input.type !== "hidden" && input.value.length < 1) {
-            is_valid = false;
-        }
-        if (input.type === "email" && !input.value.match(validRegex)) {
-            is_valid = false;
+        if (!fieldIsValid(input)) {
+            is_valid = false
         }
     });
 
     textInputs.forEach(input => {
-        if (input.type !== "submit" && input.type !== "hidden" && input.value.length < 1) {
-            is_valid = false;
+        if (!fieldIsValid(input)) {
+            is_valid = false
         }
     });
 
     let submitButton = document.getElementById("submit-contact-form");
+    submitButtonEnabled(submitButton, is_valid)
+}
 
-    // Conditionally disable and enable submit button
+// Enable submit when appropriate.
+function submitButtonEnabled(submitButton, is_valid) {
+    let message = document.getElementById("contact-form-msg")
     if (is_valid) {
         submitButton.disabled = false;
         submitButton.style = "background-color: #2a2a2a; pointer-events: all;"
-    }
-
-    else {
+    } else {
         submitButton.disabled = true;
         submitButton.style = "background-color: #dcdcdc; pointer-events: none;"
+    }
+}
+
+// Check form field is valid.
+function fieldIsValid(input) {
+    if (input.type !== "submit" && input.type !== "hidden" && input.value.length < 1) {
+        return false;
+    }
+    if (input.type === "email") {
+        if (!validEmail(input)) {
+            return false;
+        }
+    }
+    return true
+}
+
+// Change field colour based on validity.
+function fieldIsValidColor(input) {
+    if(fieldIsValid(input) || input.value.length == 0) {
+        input.style.color = "black"
+        input.style.borderBottom = "1px solid #2a2a2a";
+    }
+    else if(input.value.length > 0) {
+        input.style.color = "red"
+        input.style.borderBottom = "1px solid red";
+    }
+}
+
+// Reset the field colour on focus
+function resetColor(input) {
+    input.style.color = "black"
+    input.style.borderBottom = "1px solid #2a2a2a";
+}
+
+// Check email validity
+function validEmail(input) {
+    let validRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+    if (input.value.match(validRegex)) {
+        return true
+    } else {
+        return false
     }
 }
 
