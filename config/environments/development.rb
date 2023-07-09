87acef265d9e06eef1bde84cdeb5043a41d5241e
Steven-Klavins <stevenklavins94@gmail.com>
2023-07-09 04:23:13 +0100

    added default url for dev mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -77,4 +77,8 @@ Rails.application.configure do
 
   # Sidekiq background jobs
   config.active_job.queue_adapter = :sidekiq
+
+  # Default Url for Devise
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
 end
