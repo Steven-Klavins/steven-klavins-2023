b15a5f72a596663e3a3defd9a9e5ae8e685aeb3c
Steven-Klavins <stevenklavins94@gmail.com>
2023-07-14 23:08:53 +0100

    manually add pages

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -4,6 +4,10 @@ SitemapGenerator::Sitemap.ping_search_engines
 
 SitemapGenerator::Sitemap.create do
 
+  add blogs_path, :changefreq => 'weekly'
+  add contact_path, :changefreq => 'weekly'
+  add about_path, :changefreq => 'weekly'
+
   Blog.find_each do |blog|
     add(blog_path(blog),
         :lastmod => blog.updated_at,
