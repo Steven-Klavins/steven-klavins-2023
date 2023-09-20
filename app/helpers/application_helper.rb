1f94e38d184fadbf3fa0c36bf33373344fe1c4da
Steven-Klavins <stevenklavins94@gmail.com>
2023-09-20 22:30:23 +0100

    added taml description helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -24,8 +24,22 @@ module ApplicationHelper
 
   # Return my total years of experience.
   def years_experience
-    start_date = DateTime.new(2021,2)
+    start_date = DateTime.new(2021, 2)
     months = (Time.now.utc.to_date.year * 12 + Time.now.utc.to_date.month) - (start_date.year * 12 + start_date.month)
     months / 12
+  end
+
+  # Retrieve YAML descriptions, if a value is nil or the file cant be retrieved return 'Description unavailable...'
+  def get_description(page, section)
+    begin
+      descriptions = YAML.load(File.read("#{Rails.root}/descriptions.yml"))
+      if descriptions[page][section] != nil && descriptions[page][section] != ""
+        return descriptions[page][section]
+      else
+        return "Description unavailable..."
+      end
+    end
+  rescue
+    return "Descriptions unavailable..."
   end
 end
