8c82fb03d7aafb9c1165c39fcf01383f516c05f6
Steven-Klavins <stevenklavins94@gmail.com>
2023-10-07 00:24:27 +0100

    imported stimulus-content-loader

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,9 +2,10 @@
 
 pin "application", preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
+pin "@hotwired/stimulus", to: "https://ga.jspm.io/npm:@hotwired/stimulus@3.2.2/dist/stimulus.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 pin "slim-select", to: "https://ga.jspm.io/npm:slim-select@2.5.0/dist/slimselect.es.js"
+pin "stimulus-content-loader", to: "https://ga.jspm.io/npm:stimulus-content-loader@4.2.0/dist/stimulus-content-loader.mjs"
