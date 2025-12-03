e61873c9d93187355d69e758b9ca7e740f0e0feb
steven-klavins <stevenklavins94@gmail.com>
2025-12-03 06:06:49 +0000

    alow ongoing experiences and added date formatting

--- app/models/experience.rb
+++ app/models/experience.rb
@@ -1,6 +1,19 @@
 class Experience < ApplicationRecord
   has_one_attached :company_logo, dependent: :destroy
-  validates_presence_of :company, :position, :description, :start_date, :end_date
+  validates_presence_of :company, :position, :description, :start_date
   validates :company_logo, presence: true
 
+
+  def ongoing?
+    end_date.nil?
+  end
+
+  # Helper method to format dates for display
+  def display_dates
+    if ongoing?
+      "#{start_date.strftime('%m/%Y')} - Present"
+    else
+      "#{start_date.strftime('%m/%Y')} - #{end_date.strftime('%m/%Y')}"
+    end
+  end
 end
