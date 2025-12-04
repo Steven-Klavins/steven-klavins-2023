a1cc6ac8460f7d757701ede24166a6510e9c5c06
steven-klavins <stevenklavins94@gmail.com>
2025-12-04 22:41:53 +0000

    created a contact form class for validations

---
+++ app/forms/contact_form.rb
@@ -0,0 +1,9 @@
+# Mimic active record validations for contact form.
+class ContactForm
+  include ActiveModel::Model
+
+  attr_accessor :name, :subject, :email, :message
+
+  validates :name, :subject, :email, :message, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+end
